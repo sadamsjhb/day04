3cfeaf274d2a50ce72f5389b2808e62bab5a3623
shelton Adams <sadams@c4r9s3.wethinkcode.co.za>
2020-06-25 09:27:09 +0200

    exercise 01

--- ex01/ft_recursive_factorial.c
+++ ex01/ft_recursive_factorial.c
@@ -6,9 +6,33 @@
 /*   By: sadams <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/24 16:04:40 by sadams            #+#    #+#             */
-/*   Updated: 2020/06/24 16:26:58 by sadams           ###   ########.fr       */
+/*   Updated: 2020/06/25 09:26:25 by sadams           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
+int ft_recursive_factorial(int nb)
+{
+	if(nb < 0 || nb >= 12)
+	{
+		return (0);
+	}
+	if(nb >= 1)
+	{
+		return (nb * ft_recursive_factorial(nb -1));
+	}
+	else
+	{
+		return (1);
+	}
+}
+/*
+int main()
+{
+	int num;
+	num = 5;
+
+	printf("%d\n", ft_recursive_factorial(num));
+	return (0);
+}*/
